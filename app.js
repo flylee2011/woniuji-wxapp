d13c285af37b5c7e19c813e59c6c17af8797e8cc
liyifei <liyifei@le.com>
2017-06-26 19:28:58 +0800

    update wxml

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-//app.js
+// app.js
 App({
+  // 初始化
   onLaunch: function() {
-    //调用API从本地缓存中获取数据
+    // 调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
   },
-
+  // app 显示
+  onShow: function() {
+    console.log('app show');
+  },
+  // app 隐藏
+  onHide: function() {
+    console.log('app hide');
+  },
+  // 错误监听
+  onError: function() {
+    console.log('app error');
+  },
+  // 自定义方法
+  // 获取微信用户数据
   getUserInfo: function(cb) {
     var that = this
     if (this.globalData.userInfo) {
@@ -23,8 +37,15 @@ App({
       })
     }
   },
-
+  // 全局数据
   globalData: {
-    userInfo: null
+    // 调试
+    debug: true,
+    userInfo: null,
+    // 接口域名
+    apiDomainDebug: 'http://local.woniuji.cn',
+    apiDomain: 'https://woniuji.cn',
+    // 分享数据
+    shareTitle: '蜗牛记-让世界看到你的坚持'
   }
-})
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,41 @@
-//index.js
-//获取应用实例
+// index.js
+// 获取应用实例
 var app = getApp()
+
+// 数据
+var pageData = {
+  motto: 'Hello World',
+  userInfo: {},
+  diaryList: [{}, {}, {}]
+};
+
+// 注册页面
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {}
+  data: pageData,
+  // 页面加载
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
   },
-  //事件处理函数
-  bindViewTap: function() {
+  // 初次渲染完成
+  onReady: function() {
+    console.log('curPage: ', getCurrentPages());
+  },
+  // 分享
+  onShareAppMessage: function() {
+    return {
+      title: app.globalData.shareTitle
+    }
+  },
+  // 自定义
+  bindViewTap: function () {
     // wx.navigateTo({
     //   url: '../logs/logs'
     // })
@@ -21,16 +49,5 @@ Page({
         console.log(res);
       }
     });
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
   }
-})
+});
